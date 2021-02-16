48568555ba9fca10b1d1bf6ba2eed31d214879ca
AYehia0 <ahmedyehia611@gmail.com>
2021-02-16 22:39:04 +0200

    notify-send now works in crontab

--- remind_me.sh
+++ remind_me.sh
@@ -1,19 +1,20 @@
-#!/bin/bash
+#!/bin/sh
 
-#getting the path, another way ,, instead of using absolute path
+# getting the path, another way ,, instead of using absolute path
 parent_path=$( cd "$(dirname "${BASH_SOURCE[0]}")" ; pwd -P )
 
 #cd "$parent_path"
 
-#get the lines
+# get the lines
 date=$(vnstat -d 1 --style 0 | sed -n 6p | awk '{print $1}' )
 total=$(vnstat -d 1 --style 0 | sed -n 6p | awk '{print $2, $3 }')
 
 output="Date:${date} Usage:${total}"
 
-#saving to a file, using absolute path 
+# saving to a file, using absolute path 
 echo $output >> ~/Desktop/github/Internet-monitor/usage.txt
 
-notify-send "$output"
+# add whoami , instead of none
+sudo -u none DISPLAY=:0 DBUS_SESSION_BUS_ADDRESS=unix:path=/run/user/1000/bus notify-send "$output" 
 
 
