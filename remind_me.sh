ccfc9845cea726a7eee06b913841c9ff633d1467
AYehia0 <ahmedyehia611@gmail.com>
2021-02-16 21:29:51 +0200

    saving to a file

--- remind_me.sh
+++ remind_me.sh
@@ -1,8 +1,19 @@
 #!/bin/bash
 
+#getting the path, another way ,, instead of using absolute path
+parent_path=$( cd "$(dirname "${BASH_SOURCE[0]}")" ; pwd -P )
+
+#cd "$parent_path"
+
 #get the lines
 date=$(vnstat -d 1 --style 0 | sed -n 6p | awk '{print $1}' )
 total=$(vnstat -d 1 --style 0 | sed -n 6p | awk '{print $2, $3 }')
 
 output="Date:${date} Usage:${total}"
+
+#saving to a file, using absolute path 
+echo $output >> ~/Desktop/github/Internet-monitor/usage.txt
+
 notify-send "$output"
+
+
