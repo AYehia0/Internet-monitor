5807df6b603fa3bbca6a43d93608d7909b5eb237
AYehia0 <ahmedyehia611@gmail.com>
2021-02-16 21:02:12 +0200

    Initial Commit
    i am just getting started with scripting

---
+++ remind_me.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+#get the lines
+date=$(vnstat -d 1 --style 0 | sed -n 6p | awk '{print $1}' )
+total=$(vnstat -d 1 --style 0 | sed -n 6p | awk '{print $2, $3 }')
+
+output="Date:${date}, Usage:${total}"
+echo $output
