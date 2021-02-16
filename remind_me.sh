8e913767914a0174143ce8334a0a49adb85c0b1f
AYehia0 <ahmedyehia611@gmail.com>
2021-02-16 21:19:00 +0200

    notify me

--- remind_me.sh
+++ remind_me.sh
@@ -4,5 +4,5 @@
 date=$(vnstat -d 1 --style 0 | sed -n 6p | awk '{print $1}' )
 total=$(vnstat -d 1 --style 0 | sed -n 6p | awk '{print $2, $3 }')
 
-output="Date:${date}, Usage:${total}"
-echo $output
+output="Date:${date} Usage:${total}"
+notify-send "$output"
